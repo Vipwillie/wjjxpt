e08729b277a89ad3bac388b00565bc2fc62ab4e2
Willie <vip_willie@sina.com>
2018-04-18 10:14:47 +0800

    调整contoller层aop配置

--- springmvc/src/main/java/com/willie/springmvc/base/aop/BaseExceptionHandler.java
+++ springmvc/src/main/java/com/willie/springmvc/base/aop/BaseExceptionHandler.java
@@ -2,16 +2,27 @@ package com.willie.springmvc.base.aop;
 
 import com.willie.springmvc.base.Result;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * <p>功能 描述:使用AOP统一处理方法返回信息</p>
  * <p>创  建 人:Willie</p>
  * <p>创建 时间:2018/3/5 10:06</p>
  */
+@Aspect
+@Component
 public class BaseExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(BaseExceptionHandler.class);
+
+    @Pointcut("execution(* com.willie.springmvc.controller.*.*.*(..))")
+    public void cutPoint() {
+    }
 
     /**
      * 处理Controller中的方法
@@ -19,13 +30,14 @@ public class BaseExceptionHandler {
      * @param proceedingJoinPoint
      * @return
      */
+    @Around("cutPoint()")
     public Object handlerControllerMethod(ProceedingJoinPoint proceedingJoinPoint) {
         long startTime = System.currentTimeMillis();
         Result<?> result = null;
         try {
             result = (Result<?>) proceedingJoinPoint.proceed();
             long endTime = System.currentTimeMillis();
-            logger.info("used time:[{}]毫秒", endTime-startTime);
+            logger.info("used time:[{}]毫秒", endTime - startTime);
         } catch (Throwable throwable) {
             result = handlerException(proceedingJoinPoint, throwable);
         }
@@ -39,6 +51,7 @@ public class BaseExceptionHandler {
      * @param throwable
      * @return
      */
+    @AfterThrowing(value = "cutPoint()",throwing="throwable")
     private Result<?> handlerException(ProceedingJoinPoint pjp, Throwable throwable) {
         Result<?> result = new Result();
         //已知异常
